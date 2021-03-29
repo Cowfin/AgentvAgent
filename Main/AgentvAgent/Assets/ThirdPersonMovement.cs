e57a19def1b14f4f893b16808a6934d034790f6e
Kelvin Wong <kakoo40@gmail.com>
2021-03-29 15:29:18 +1300

    Player Movement

---
+++ Main/AgentvAgent/Assets/ThirdPersonMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+
+    public CharacterController controller;
+    public Transform cam; //Takes camera looking direction
+
+    public float speed = 6f;
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal"); //If D/Right = 1, if A/Left = -1
+        float vertical = Input.GetAxisRaw("Vertical"); //If W/Up = 1, if S/Down = -1
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y; //Finds the angle of movement
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime); //Smooths the player rotation so it is not snappy
+            transform.rotation = Quaternion.Euler(0f, angle, 0f); //Rotates the player to the angle of movement
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime); //Moves the player
+        }
+    }
+}
