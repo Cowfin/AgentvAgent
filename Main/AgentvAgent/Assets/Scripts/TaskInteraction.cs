6fe613fc7425fcccce686dfeee082d6865dbc18b
Kelvin Wong <kakoo40@gmail.com>
2021-04-05 11:14:34 +1200

    Progress on task interaction and pop up

---
+++ Main/AgentvAgent/Assets/Scripts/TaskInteraction.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TaskInteraction : MonoBehaviour 
+{
+    [SerializeField] Image taskPopup;
+
+    float interactRange = 3f;
+
+    int taskLayerMask;
+
+    Camera cam;
+
+    GameObject gameController;
+    TaskDatabase database;
+    TaskTimer taskTimer;
+
+    void Start()
+    {
+        gameController = GameObject.FindGameObjectWithTag("GameController");
+        database = gameController.GetComponent<TaskDatabase>();
+        cam = gameController.GetComponent<Camera>();
+
+        taskLayerMask = LayerMask.GetMask("Task");
+        DisableTaskPopUp();
+    }
+
+    void Update()
+    {
+        Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, interactRange, taskLayerMask))
+        {
+            EnableTaskPopUp();
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                int id = hit.transform.GetComponent<TaskID>().taskID;
+                taskTimer.timeRemaining = database.tasks[id].completeTime;
+                taskTimer.timerRunning = true;
+            }
+            else
+            {
+                taskTimer.timerRunning = false;
+            }
+        }
+        else
+        {
+            DisableTaskPopUp();
+        }
+
+    }
+
+    void EnableTaskPopUp()
+    {
+        taskPopup.gameObject.SetActive(true);
+    }
+
+    void DisableTaskPopUp()
+    {
+        taskPopup.gameObject.SetActive(false);
+    }
+}
---
+++ Main/AgentvAgent/Assets/Scripts/TaskTimer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TaskTimer : MonoBehaviour {
+
+    public float timeRemaining;
+    public bool timerRunning = false;
+
+    void Start() {
+        timerRunning = true;
+    }
+
+    void Update() {
+        if (timerRunning) {
+            if (timeRemaining > 0) {
+                timeRemaining -= Time.deltaTime;
+            } else {
+                timerRunning = false;
+                timeRemaining = 0;
+            }
+        }
+    }
+}
