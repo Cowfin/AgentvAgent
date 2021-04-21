c7918eebae7e7cc8b1c0ab8fd58a7cff1cc9c510
nst2000 <sennoel2000@outlook.co.nz>
2021-04-21 23:42:03 +1200

    Menuscript

---
+++ Main/AgentvAgent/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+
+
+    public class MainMenu : MonoBehaviour
+    {
+        [SerializeField]
+         private NetworkManagerLobby agentLobbyBuild = null;
+
+        [Header("UI")]
+        [SerializeField]
+        private GameObject LobbyMenu = null;
+
+        public void HostLobby()
+        {
+            agentLobbyBuild.StartHost();
+
+            LobbyMenu.SetActive(false);
+        }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+}
+
+
+
