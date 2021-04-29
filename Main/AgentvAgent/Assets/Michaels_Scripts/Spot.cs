a90dc714f2ef6aa44302e4a2e4d35ecc2af101a5
Kelvin Wong <kakoo40@gmail.com>
2021-04-29 20:10:30 +1200

    Merged Michael's Pathfinding AI
    Merged and checked the AI pathing with the mansion scene

---
+++ Main/AgentvAgent/Assets/Michaels_Scripts/Spot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spot : MonoBehaviour
+{
+    public Vector3 location;
+    public bool occupied;
+
+    void Start()
+    {
+        location = this.transform.position;
+        occupied = false;
+    }
+
+    public bool getOccupation()
+    {
+        return occupied;
+    }
+    public void changeOccupation()
+    {
+        occupied = !occupied;
+    }
+}
