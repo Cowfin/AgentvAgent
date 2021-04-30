ff709a0075a692350077162dcb3f648a80d673f9
Kelvin Wong <kakoo40@gmail.com>
2021-04-30 17:34:27 +1200

    Switching to main branch

---
+++ AgentvAgent/Assets/Michaels_Scripts/Spot.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spot : MonoBehaviour
+{
+    public Vector3 location;
+    public bool occupied;
+
+    void Start()
+    {
+        location = this.transform.position;
+        occupied = false;
+    }
+
+    public bool getOccupation()
+    {
+        return occupied;
+    }
+    public void occupationEmpty()
+    {
+        occupied = false;
+    }
+
+    public void occupationFull()
+    {
+        occupied = true;
+    }
+}
