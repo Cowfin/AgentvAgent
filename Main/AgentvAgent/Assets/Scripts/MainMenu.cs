d607b6d090631147a2463fd0e25553c6625a0ce3
nst2000 <sennoel2000@outlook.co.nz>
2021-04-30 15:57:50 +1200

    MainMenuSimple

--- Main/AgentvAgent/Assets/Scripts/MainMenu.cs
+++ Main/AgentvAgent/Assets/Scripts/MainMenu.cs
@@ -4,19 +4,7 @@
 
     public class MainMenu : MonoBehaviour
     {
-        [SerializeField]
-         private NetworkManagerLobby agentNetworkLobby = null;
-
-        [Header("UI")]
-        [SerializeField]
-        private GameObject  agentRoomMenu = null;
-
-        public void HostLobby()
-        {
-            agentNetworkLobby.StartHost();
-
-            agentRoomMenu.SetActive(false);
-        }
+     
 
     public void QuitGame()
     {
