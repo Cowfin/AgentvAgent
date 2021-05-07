b38c3f785a7c2bda2635e3bc07a653bda76b7b3c
MichaelJRitchie <80668182+MichaelJRitchie@users.noreply.github.com>
2021-05-07 16:43:47 +1200

    Vision Blackout
    Added vision blackout to hunter if they miss their shot. Hit detection not yet implemented. Added this script to the spy to test functionality although this will need to be removed later

--- Main/AgentvAgent/Assets/Kelvin-Scripts/HunterInteraction.cs
+++ Main/AgentvAgent/Assets/Kelvin-Scripts/HunterInteraction.cs
@@ -1,13 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HunterInteraction : MonoBehaviour
 {
     [SerializeField] Camera cam;
+    [SerializeField] Image tunnelVision;
+    [SerializeField] Image tunnelVisionGrey;
 
     float interactRange = 1000f;
     int taskLayerMask;
+
+    bool obscureVision = true;
+    bool fadeIn;
+    bool fadeOut;
+
+    float fadeInTime = 0.1f;
+    float holdTime = 7f;
+    float fadeOutTime = 3f;
+    float fadeTimer = 0f;
 
     GameObject gameController;
     TaskDatabase database;
@@ -29,29 +41,90 @@ public class HunterInteraction : MonoBehaviour
         Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
         RaycastHit hit;
 
-        if (Input.GetMouseButtonDown(1)) // On right click
+        if (obscureVision == true)
+        {
+            fadeTimer += Time.deltaTime;
+            if (fadeIn)
+            {
+
+                Color newTunnelVision = tunnelVision.color;
+                newTunnelVision.a = fadeTimer / fadeInTime;
+                tunnelVision.color = newTunnelVision;
+
+                Debug.Log("Fading in, alpha : " + newTunnelVision.a);
+
+                Color newTunnelVisionGrey = tunnelVisionGrey.color;
+                newTunnelVisionGrey.a = 0.6f * (fadeTimer / fadeInTime);
+                tunnelVisionGrey.color = newTunnelVisionGrey;
+
+                if (fadeTimer >= fadeInTime)
+                {
+                    fadeIn = false;
+                    fadeTimer = 0;
+                }
+            }
+            else
+            {
+                if (fadeOut == false)
+                {
+                    if (fadeTimer >= holdTime)
+                    {
+                        fadeOut = true;
+                        fadeTimer = 0;
+                    }
+                }
+                else
+                {
+                    Color newTunnelVision = tunnelVision.color;
+                    newTunnelVision.a -= fadeTimer / fadeOutTime;
+                    tunnelVision.color = newTunnelVision;
+
+                    Color newTunnelVisionGrey = tunnelVisionGrey.color;
+                    newTunnelVisionGrey.a -= 0.6f * (fadeTimer / fadeOutTime);
+                    tunnelVisionGrey.color = newTunnelVisionGrey;
+
+                    if (fadeTimer >= fadeOutTime)
+                    {
+                        fadeOut = false;
+                        fadeTimer = 0;
+                        obscureVision = false;
+                    }
+                }
+            }
+        }
+
+        if (Input.GetMouseButton(1)) // On right click
         {
             Debug.Log("Hunter Aiming");
             //Aiming
+            if (Input.GetMouseButtonDown(0))
+            {
+                //if player hit then kill player
+
+                resetTunnelVision();
+                obscureVision = true;
+                Debug.Log("Hunter Shot");
+                // shoot but no one dies
+            }
 
             if (Physics.Raycast(ray, out hit, interactRange, taskLayerMask))
             {
-                if (Input.GetMouseButton(0))
-                {
-                    //if player hit then kill player
-                } else
-                {
-                    // shoot but no one dies
-                }
                 
             }
-            
         }
-
-       
-
-
-
     }
+    void resetTunnelVision()
+    {
+        Color newTunnelVision = tunnelVision.color;
+        newTunnelVision.a = 0;
+        tunnelVision.color = newTunnelVision;
 
+        Color newTunnelVisionGrey = tunnelVisionGrey.color;
+        newTunnelVisionGrey.a = 0;
+        tunnelVisionGrey.color = newTunnelVisionGrey;
+
+        fadeIn = true;
+        fadeOut = false;
+        fadeTimer = 0f;
+    }
 }
