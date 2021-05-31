415727b37f0b32dfaf443ee7ca13d280df5149fd
Kelvin Wong <kakoo40@gmail.com>
2021-06-01 00:13:16 +1200

    Updating to latest version

---
+++ AgentvAgent/Assets/Scripts/Player/HunterMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HunterMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform cam; //Takes camera looking direction
+
+    public float speed = 6f;
+    float turnSpeed = 5f;
+    float turnSmoothVelocity;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal"); //If D/Right = 1, if A/Left = -1
+        float vertical = Input.GetAxisRaw("Vertical"); //If W/Up = 1, if S/Down = -1
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y; //Finds the angle of movement
+
+            if (targetAngle != transform.rotation.y) // Calculates the speed and direction of rotation for the player model
+            {
+                transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0f, targetAngle, 0f), Time.deltaTime * turnSpeed); //Smooths the player rotation so it is not snappy
+            }
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime); //Moves the player
+        }
+        else
+        {
+            //PLAYER IS IDLE
+        }
+
+    }
+}
---
+++ AgentvAgent/Assets/Scripts/Tests/TaskWinTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class TaskWinTest
+    {
+        [Test]
+        [TestCase(null)]
+        [TestCase(-1)]
+        [TestCase(100)]
+        [TestCase(4)]
+        [TestCase(6)]
+        [TestCase(10000)]
+        [TestCase(0)]
+        [TestCase(2)]
+
+        public void TaskWinTestSimplePasses(int tasksCompleted)
+        {
+            var gControl = new GControl();
+
+            for (int i = 0; i < tasksCompleted; i++)
+            {
+                gControl.addTaskComplete();
+            }
+
+            Assert.That(gControl.checkSpyTaskWin());
+        }
+    }
+}
